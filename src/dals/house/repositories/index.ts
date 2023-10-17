43554607af31efe584bceeacb9214ee41ab05740
Aridane Martín <aridanemartin@gmail.com>
2023-10-17 19:55:55 +0100

    repository selected by env

--- src/dals/house/repositories/index.ts
+++ src/dals/house/repositories/index.ts
@@ -2,7 +2,6 @@ import { houseMockRepository } from "./house.mock-repository.js";
 import { housedbRepository } from "./house.db-repository.js";
 import { envConstants } from "#core/constants/index.js";
 
-export const houseRepository = housedbRepository;
-// export const houseRepository = envConstants.isApiMock
-//   ? houseMockRepository
-//   : housedbRepository;
+export const houseRepository = envConstants.isApiMock
+  ? houseMockRepository
+  : housedbRepository;
